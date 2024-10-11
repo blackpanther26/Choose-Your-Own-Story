51c3166a9f8bd47699424f516f84d3ccab34d0e8
blackpanther26 <priyanshuchahal1@gmail.com>
2024-10-11 18:42:52 +0530

    Initial Commit

---
+++ Choose Your Own Story/Choose_Your_Own_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_Your_Own_StoryApp.swift
+//  Choose Your Own Story
+//
+//  Created by Priyanshu Chahal on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Choose_Your_Own_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Choose Your Own Story/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Choose Your Own Story
+//
+//  Created by Priyanshu Chahal on 11/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
